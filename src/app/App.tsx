5b1bbb93a0f71fade406d6499b8a5c0a58e3a9a2
maksie <nekroot@mail.ru>
2023-02-18 15:07:18 +0300

    add Navbar and AppLink components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,16 @@ import {Link, Route, Routes} from 'react-router-dom'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/provider";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
         </div>
     )
 }
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,26 @@
+import React, {FC} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './AppLink.module.scss';
+import {Link, LinkProps} from "react-router-dom";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps{
+    className?: string;
+    theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = ({to, className,children,theme = AppLinkTheme.PRIMARY, ...otherProps}) => {
+
+    return (
+        <Link to={to}
+              className={classNames(cls.AppLink, {[cls[theme]]: true}, [className])}
+              {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+}

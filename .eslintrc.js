8c6dd7265989ef473c61a102f0e850221f58c2f7
maksie <nekroot@mail.ru>
2023-03-08 11:53:56 +0300

    add stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
 	"extends": [
 		"eslint:recommended",
 		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
+		"plugin:i18next/recommended"
 	],
 	"overrides": [
 	],
@@ -17,7 +18,8 @@ module.exports = {
 	},
 	"plugins": [
 		"react",
-		"@typescript-eslint"
+		"@typescript-eslint",
+		"i18next"
 	],
 	"rules": {
 		"indent": [
@@ -36,7 +38,8 @@ module.exports = {
 			"error",
 			"always"
 		],
-		"react/react-in-jsx-scope": "off"
+		"react/react-in-jsx-scope": "off",
+		"i18next/no-literal-string": ["error", {markupOnly: true}],
 	},
 	globals: {
 		"__IS_DEV__": true
